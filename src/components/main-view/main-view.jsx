795e6f3f0e6b7564e7eff78d8a90718d9ebae578
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-20 12:37:33 -0500

    console.log the api data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -7,6 +7,23 @@ export const MainView = () => {
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  useEffect(() => {
+    fetch("https://straberryoctosquid-1858bcf4dbcb.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("moviesFromApi", data);
+        //   const moviesFromApi = data.docs.map((doc) => {
+        //     return {
+        //       id: doc.key,
+        //       title: doc.title,
+        //       image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+        //       author: doc.author_name?.[0]
+        //     };
+        //   });
+
+        //   setMovies(moviesFromApi);
+      });
+  }, []);
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -14,7 +31,7 @@ export const MainView = () => {
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return <div>The list is empty!!</div>;
   }
 
   return (
