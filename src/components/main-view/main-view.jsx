19c0a272be55fe9e42ee22578835899ef7ef3f03
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-24 09:56:30 -0500

    Gain basic functionality

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,86 +2,94 @@ import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
-
+  const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
 
-  if (!user) {
-    return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-    );
-  }
+
 
   useEffect(() => {
-    if (!token) {
+    if (!token)
       return;
-    }
 
-    fetch("  https://straberryoctosquid-1858bcf4dbcb.herokuapp.com/movies", {
+    fetch("https://straberryoctosquid-1858bcf4dbcb.herokuapp.com/movies", {
       headers: { Authorization: `Bearer ${token}` }
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+
+
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie._id,
+            Title: movie.Title,
+            ImagePath: movie.ImagePath,
+            Description: movie.Description,
+            Actors: [movie.Actors],
+            Genre: {
+              Name: movie.Genre.Name,
+              Description: movie.Genre.Description,
+            },
+            Director: {
+              Name: movie.Director.Name,
+              Bio: movie.Director.Bio,
+            },
+            Featured: movie.Featured,
+          };
+        });
+        setMovies(moviesFromApi);
+      })
+
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
       });
   }, [token]);
 
-  const moviesFromApi = data.map((movie) => {
-    return {
-      _id: movie._id,
-      Title: movie.Title,
-      ImagePath: movie.ImagePath,
-      Description: movie.Description,
-      Actors: [movie.Actors],
-      Genre: {
-        Name: movie.Genre.Name,
-        Description: movie.Genre.Description,
-      },
-      Director: {
-        Name: movie.Director.Name,
-        Bio: movie.Director.Bio,
-      },
-      Featured: movie.Featured,
-
-    };
-  });
-
-  setMovies(moviesFromApi);
-};
-[];
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
 
-if (selectedMovie) {
+  if (selectedMovie) {
+    return (
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!!</div>;
+  }
+
   return (
-    <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie._id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+    </div>
   );
-}
-
-if (movies.length === 0) {
-  return <div>The list is empty!!</div>;
-}
-
-return (
-  <div>
-    {movies.map((movie) => (
-      <MovieCard
-        key={movie._id}
-        movie={movie}
-        onMovieClick={(newSelectedMovie) => {
-          setSelectedMovie(newSelectedMovie);
-        }}
-      />
-    ))}
-    <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
-  </div>
-);
-
+};
