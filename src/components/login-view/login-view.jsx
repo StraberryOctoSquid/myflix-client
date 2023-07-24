b722ff882bf58240ad4bbb162387b43533f3f05b
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-24 07:51:41 -0500

    create login-view structure

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password
+    };
+
+    fetch("https://straberryoctosquid-1858bcf4dbcb.herokuapp.com/login", {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="9"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
