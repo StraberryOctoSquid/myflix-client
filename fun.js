555e898fedc361a6b0e990e78f3ebe14e0255335
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-26 09:46:23 -0500

    Update fun.js

---
+++ fun.js
@@ -0,0 +1,29 @@
+// q: what is object oriented programming?
+// a: object oriented programming is a programming paradigm that uses objects and their interactions to design applications and computer programs.
+// q: what is functional programming?
+// a: functional programming is a programming paradigm that uses functions to design applications and computer programs.
+// q: what is the difference between object oriented programming and functional programming?
+// a: object oriented programming is about objects and their interactions, while functional programming is about functions and their interactions.
+// q: What are you?
+// a: I am a function.
+// q: What is your name?
+// a: I don't have a name.
+// q: What is your purpose?
+// a: I don't have a purpose.
+// q: What is your favorite programming language?
+// a: I don't have a favorite programming language.
+// q: Do you think this is funny?
+// a: I don't have a sense of humor.
+// q: Do you think I am funny?
+// a: I don't have a sense of humor.
+// q: Do you think I am stupid?
+// a: I don't have a sense of humor.
+// q: Do you think I am smart?
+// a: I don't have a sense of humor.
+// q: Do you think I am pretty?
+// a: I don't have a sense of humor.
+// q: Do you think?
+
+
+
+
