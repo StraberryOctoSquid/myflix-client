f880ce5935b69d125cc776bc793de449a6e01e4e
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-27 07:22:10 -0500

    wrap login form in card

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -40,23 +40,36 @@ export const LoginView = ({ onLoggedIn }) => {
     <Container>
       <Row className="justify-content-md-center">
         <Col md={6}>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicUsername">
-              <Form.Label>Username:</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} required minLength="3" />:
-              <Form.Text className="text-muted">
-                We'll never share your username with anyone else.
-              </Form.Text>
-            </Form.Group>
+          <CardGroup>
+            <Card className="login-card">
+              <Card.Title className="login-title">myFlix</Card.Title>
+              {/* <Card.Header className="login-header">Welcome to the registration page!</Card.Header> */}
+              <Card.Body>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password:</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required minLength="3" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form >
+
+
+
+                <Form onSubmit={handleSubmit}>
+
+                  <Form.Group className="mb-3" controlId="formBasicUsername">
+                    {/* <Form.Label>Username:</Form.Label> */}
+                    <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} required minLength="3" />
+                    {/* <Form.Text className="text-muted">
+                    We'll never share your username with anyone else.
+                  </Form.Text> */}
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    {/* <Form.Label>Password:</Form.Label> */}
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required minLength="3" />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Sign In
+                  </Button>
+                </Form >
+              </Card.Body>
+            </Card>
+          </CardGroup>
         </Col>
       </Row>
     </Container>
