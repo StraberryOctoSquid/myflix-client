e0f5d2385583ec9cd0a6b62a353470588f2c406e
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-27 10:07:22 -0500

    Add col and row to movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,30 +1,39 @@
-// How does the back button work?
-// ?Where does onBackClick get it's powers? Main view?
 import "./movie-view.scss";
+import { Col, Row } from "react-bootstrap";
+
+
 export const MovieView = ({ movie, onBackClick }) => {
+
   return (
-    <div>
-      <button
-        onClick={onBackClick}
-        className="back-button"
-        style={{ cursor: "pointer" }}
-      >
-        Back</button>
-      <div>
-        <img src={movie.ImagePath} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.Title}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.Director.Name} </span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.Description} </span>
-      </div>
-    </div>
+    <Col md={8} style={{ border: "1px solid black" }}>
+      <Row className="justify-content-md-center">
+        <div>
+          <button
+            onClick={onBackClick}
+            className="back-button"
+            style={{ cursor: "pointer" }}
+          >
+            Back</button>
+          <div>
+            <img
+              src={movie.ImagePath}
+              className="movie-poster"
+            />
+          </div>
+          <div>
+            <span>Title: </span>
+            <span>{movie.Title}</span>
+          </div>
+          <div>
+            <span>Director: </span>
+            <span>{movie.Director.Name} </span>
+          </div>
+          <div>
+            <span>Description: </span>
+            <span>{movie.Description} </span>
+          </div>
+        </div>
+      </Row>
+    </Col>
   );
 };
