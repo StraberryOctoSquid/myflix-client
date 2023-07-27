075ca577e222dda00337f06febf722e7b4d9d00b
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-27 12:14:02 -0500

    Remove Red boarder

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -37,7 +37,7 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <Container style={{ border: "1px solid red " }}>
+    <Container >
       <Row className="justify-content-md-center">
         <Col md={6}>
           <CardGroup>
