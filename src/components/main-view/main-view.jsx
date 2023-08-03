9df6156bf52b3b5eac0570b580cad2025d5235ac
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-08-03 07:14:24 -0500

    Clean notes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,25 +1,17 @@
 import { useEffect, useState } from "react";
-// MovieCard formats how movie information should be loaded and displayed
 import { MovieCard } from "../movie-card/movie-card";
-// MovieView formats the page when a movie is clicked on to display that movie's data.
 import { MovieView } from "../movie-view/movie-view";
-// LoginView creates a log in form for the client to use their credentials to log in. The form requires certain validation
 import { LoginView } from "../login-view/login-view";
-// Signupview creates a form for new users to sign up, it will store their information in the server, it requires validation.
 import { SignupView } from "../signup-view/signup-view";
-import "./main-view.scss";
+// import "./main-view.scss";
 import { Col, Nav, Row } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const MainView = () => {
-  // store the client's user name in local storage
-  // Q: Where is storedUser supposed to be used?
   const storedUser = JSON.parse(localStorage.getItem("user"));
-  // Store the client's jwt token
   const storedToken = localStorage.getItem("token");
-  // ?create variables with state hooks and their default values?
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  // const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
 
@@ -72,49 +64,13 @@ export const MainView = () => {
     <BrowserRouter>
       <Row className="justify-content-md-center">
         <Routes>
-          {/* <Route
-            path="/"
-            element={
-              <>
-                {user ? (
-                  <Navigate to="/movies" />
-                ) : (
-                  <Col md={5}>
-                    <LoginView />
-                  </Col>
-                )}
-              </>
-            }
-          /> */}
-
-          {//If there is a user navigate to movies, if not navigate to login
-          }
-          <Route
-            path="/"
-            element={
-              <>
-                {user ? (
-                  <Navigate to="/movies" />
-                ) : (
-                  <Col md={5}>
-                    <LoginView
-                      onLoggedIn={(user, token) => {
-                        setUser(user);
-                        setToken(token);
-                      }} />
-                  </Col>
-                )}
-              </>
-            }
-          />
-
 
           <Route
             path="/signup"
             element={
               <>
                 {user ? (
-                  <Navigate to="</>" />
+                  <Navigate to="/" />
                 ) : (
                   <Col md={5}>
                     <SignupView />
@@ -135,6 +91,7 @@ export const MainView = () => {
                     <LoginView
                       onLoggedIn={(user, token) => {
                         setUser(user);
+                        console.log("user", user);
                         setToken(token);
                       }}
                     />
@@ -148,16 +105,39 @@ export const MainView = () => {
             path="/movies/:movieId"
             element={
               <>
-                {user ? (
+                {!user ? (
                   <Navigate to="/login" replace />
                 ) : movies.length === 0 ? (
                   <Col> The List is Empty!</Col>
                 ) : (
                   <Col md={8}>
-                    {/* maybe this should be movie={movie} */}
-                    <MovieView movie={selectedMovie} />
+                    <MovieView movies={movies} />
                   </Col>
                 )}
+              </>
+            }
+          />
+
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  // Show movies
+                  <Navigate to="/login" replace />
+                ) :
+                  movies.length === 0 ? (
+                    <Col> The List is Empty!</Col>
+                  ) : (
+                    <>
+                      {movies.map((movie) => (
+                        <Col className="mb-4 key={movie._id}" md={3}>
+                          <MovieCard movie={movie} />
+                        </Col>
+                      ))}
+                    </>
+                  )
+                }
               </>
             }
           />
@@ -167,4 +147,3 @@ export const MainView = () => {
     </BrowserRouter>
   );
 };
-
