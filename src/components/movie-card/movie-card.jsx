e737bc74dc3e1bed1956c7b56118bfb4d4556a74
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-08-03 10:42:55 -0500

    Make movie-card a link
    remove text decoration

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,16 +5,23 @@ import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie }) => {
   return (
-    <Card>
-      <Card.Img variant="top" src={movie.ImagePath} />
-      <Card.Body>
-        <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Description}</Card.Text>
-        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+    <Link to={`/movies/${encodeURIComponent(movie._id)}`} style={{ textDecoration: "none" }}>
+      <Card>
+        {/* <Card.Img variant="top" src={movie.ImagePath} /> */}
+
+
+        <Card.Img variant="top" src={movie.ImagePath} />
+
+
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          {/* <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
           <Button variant="link">Open</Button>
-        </Link>
-      </Card.Body>
-    </Card>
+        </Link> */}
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
