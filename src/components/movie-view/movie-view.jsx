e5530454daae94f5ba2b61f220f012839f5a17d1
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-08-03 11:13:56 -0500

    Tidy up movie view links
    The back button is gone. The entire div is now a clickable link that acts as the back button. The text formatting has been removed and padding added.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,28 +9,30 @@ export const MovieView = ({ movies }) => {
   const movie = movies.find((m) => m._id === movieId);
 
   return (
-    <div>
+    <Link to={`/`} style={{ textDecoration: "none", color: "black" }} className="movie-poster">
       <div>
-        <img
-          className="w-100"
-          src={movie.ImagePath}
-        />
+        <div>
+          <img
+            className="w-100"
+            src={movie.ImagePath}
+          />
+        </div>
+        <div>
+          <span>Title: </span>
+          <span>{movie.Title}</span>
+        </div>
+        <div>
+          <span>Director: </span>
+          <span>{movie.Director.Name} </span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.Description} </span>
+        </div>
+
+        {/* <button className="back-button">Back</button> */}
+
       </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.Title}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.Director.Name} </span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.Description} </span>
-      </div>
-      <Link to={`/`}>
-        <button className="back-button">Back</button>
-      </Link>
-    </div>
+    </Link>
   );
 };
