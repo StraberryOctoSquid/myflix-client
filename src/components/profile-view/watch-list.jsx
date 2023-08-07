3296ae6a2949631e4201c1f08f516935783bb0b5
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-08-07 13:29:05 -0500

    Create watch-list.jsx

---
+++ src/components/profile-view/watch-list.jsx
@@ -0,0 +1,23 @@
+import { Row, Col } from 'react-bootstrap';
+import { MovieCard } from '../movie-card/movie-card';
+
+export const WatchList = ({ user, token, movie, updatedUser }) => {
+  let result = movie.filter((movie) => user.FavoriteMovies.includes(movie._id));
+
+  return (
+    <>
+      <Row>
+        {result.map((movie) => (
+          <Col className='mb-4' md={3}>
+            <MovieCard
+              movie={movie}
+              user={user}
+              updatedUser={updatedUser}
+              token={token}
+            />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
