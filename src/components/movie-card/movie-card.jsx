3d6f270dabf6b7254d53b537ba6d74432d77cede
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-08-10 10:18:52 -0500

    Creating button to add or remove favorite movie

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,9 +3,30 @@ import PropTypes from "prop-types";
 import { Button, Card } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import { addFavorite } from "../movie-card/addFavorite.jsx"
+import { useState } from "react";
 
 export const MovieCard = ({ movie, token, user }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  // console.log("user", user);
+  // console.log("user.FavoriteMovies", user.FavoriteMovies);
+  // const [x, setX] = useState("one");
+
+  // const checkFavorite = (movie, user) => {
+
+  //   if (user.FavoriteMovies.includes(movie._id)) {
+
+  //     setX("Remove from Favorites");
+  //   } else {
+  //     setX("Add to Favorites");
+  //   }
+
+  // }
+
+  // checkFavorite(movie, user);
+
   return (
+
     < Card >
 
       <Card.Img variant="top" src={movie.ImagePath} />
@@ -18,10 +39,18 @@ export const MovieCard = ({ movie, token, user }) => {
           <Button variant="link">Open</Button>
         </Link>
         {/* create a button that allows a user to add a movie to user.favorites */}
-        <Button variant="link" onClick={() => addFavorite(movie, token, user)}>Add to Favorites</Button>
+
+        {isFavorite ? (
+          <Button variant="link" onClick={() => removeFavorite(movie, token, user)}>Remove from Favorites</Button>
+        ) : (
+          <Button variant="link" onClick={() => addFavorite(movie, token, user)}>Add to Favorites</Button>
+        )}
+        {/* <Button variant="link" onClick={() => addFavorite(movie, token, user)}>{x}</Button> */}
 
 
       </Card.Body>
+
+
     </Card >
   );
 };
@@ -37,3 +66,8 @@ MovieCard.propTypes = {
     _id: PropTypes.string,
   }).isRequired,
 };
+
+// create a function that checks weather or not a movie is in the user.favorites array
+// if it is not in the array, change the text of the button to "Add to Favorites"
+// if it is in the array, change the text of the button to "Remove from Favorites"
+
