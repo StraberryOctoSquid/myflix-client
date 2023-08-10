46025b07f7d64ac3e069977025a015de217b7c5a
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-08-10 12:46:29 -0500

    Fix isFavorite

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,84 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import { addFavorite } from "../movie-card/addFavorite.jsx"
 import { useState } from "react";
 
-export const MovieCard = ({ movie, token, user, setUser }) => {
-  const removeFavorite = (movie, token, user, setUser) => {
-    console.log("removeFavorite");
-    fetch(`https://straberryoctosquid-1858bcf4dbcb.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => {
-        if (res.ok) {
-          console.log("removed from favorite");
-          return res.json();
-        }
-      }).then((data) => {
-        console.log("Data:", data);
-        // setIsFavorite(false);
-        setUser(data);
-        alert("Removed from Favorites");
+export
+  const MovieCard = ({ movie, token, user, setUser }) => {
+
+    // I need to better understand what is happening here.???
+    useEffect(() => {
+      const isFavorited = user.FavoriteMovies.includes(movie._id);
+      setIsFavorite(isFavorited);
+    }, [user, movie._id]);
+
+
+    const removeFavorite = (movie, token, user, setUser) => {
+      fetch(`https://straberryoctosquid-1858bcf4dbcb.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then((res) => {
+          if (res.ok) {
+            console.log("removed from favorite");
+            return res.json();
+          }
+        }).then((data) => {
+          console.log("Data:", data);
+          setIsFavorite(false);
+          setUser(data);
+          alert("Removed from Favorites");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    const [isFavorite, setIsFavorite] = useState(false);
+
+
+    return (
+
+      < Card >
+
+        <Card.Img variant="top" src={movie.ImagePath} />
+
+
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+            <Button variant="link">Open</Button>
+          </Link>
+          {/* create a button that allows a user to add a movie to user.favorites */}
+
+          {isFavorite ? (
+            <Button variant="link" onClick={() => removeFavorite(movie, token, user, setUser)}>Remove from Favorites</Button>
+          ) : (
+            <Button variant="link" onClick={() => addFavorite(movie, token, user, setUser, setIsFavorite)}>Add to Favorites</Button>
+          )}
+          {/* <Button variant="link" onClick={() => addFavorite(movie, token, user)}>{x}</Button> */}
+
+
+        </Card.Body>
+
+
+      </Card >
+    );
   };
-
-  const [isFavorite, setIsFavorite] = useState(true);
-
-  // console.log("user", user);
-  // console.log("user.FavoriteMovies", user.FavoriteMovies);
-  // const [x, setX] = useState("one");
-
-  // const checkFavorite = (movie, user) => {
-
-  //   if (user.FavoriteMovies.includes(movie._id)) {
-
-  //     setX("Remove from Favorites");
-  //   } else {
-  //     setX("Add to Favorites");
-  //   }
-
-  // }
-
-  // checkFavorite(movie, user);
-
-  return (
-
-    < Card >
-
-      <Card.Img variant="top" src={movie.ImagePath} />
-
-
-      <Card.Body>
-        <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Description}</Card.Text>
-        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-          <Button variant="link">Open</Button>
-        </Link>
-        {/* create a button that allows a user to add a movie to user.favorites */}
-
-        {isFavorite ? (
-          <Button variant="link" onClick={() => removeFavorite(movie, token, user, setUser)}>Remove from Favorites</Button>
-        ) : (
-          <Button variant="link" onClick={() => addFavorite(movie, token, user, setUser)}>Add to Favorites</Button>
-        )}
-        {/* <Button variant="link" onClick={() => addFavorite(movie, token, user)}>{x}</Button> */}
-
-
-      </Card.Body>
-
-
-    </Card >
-  );
-};
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
