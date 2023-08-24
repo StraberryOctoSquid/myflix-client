74b6f393f2c36031ab2b68718c16c14c4e5dd7bb
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-08-24 11:02:46 -0500

    Improve navbar ux

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -5,32 +5,33 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
-    <Navbar bg="light" expand="lg" className="bg-body-tertiary">
+    <Navbar bg="light" expand="sm" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={Link} to="/">myFlix</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {!user && (
-              <>
-                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
-              </>
-            )}
-            {user && (
-              <>
-                <NavDropdown title="myFlix" id="basic-nav-dropdown">
-                  <NavDropdown.Item as={Link} to="/">Home</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/watch-list">Watch-List</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} onClick={onLoggedOut}>Logout</NavDropdown.Item>
-                </NavDropdown>
-              </>
-            )}
+        {/* <Navbar.Brand as={Link} to="/">myFlix</Navbar.Brand> */}
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+        <Nav className="me-auto">
+          {!user && (
+            <>
+              <Navbar.Brand as={Link} to="/">myFlix</Navbar.Brand>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+            </>
+          )}
+          {user && (
+            <>
+              <NavDropdown title="myFlix" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/">Home</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/watch-list">Watch-List</NavDropdown.Item>
+                <NavDropdown.Item as={Link} onClick={onLoggedOut}>Logout</NavDropdown.Item>
+              </NavDropdown>
+            </>
+          )}
 
 
-          </Nav>
-        </Navbar.Collapse>
+        </Nav>
+        {/* </Navbar.Collapse> */}
       </Container>
     </Navbar>
   );
